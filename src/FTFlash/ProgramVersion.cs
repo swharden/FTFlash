fc5a5d34f6db37391fd15b52a13823179dc3f3fe
Scott W Harden <swharden@gmail.com>
2024-03-23 12:07:52 -0400

    bump version

--- src/FTFlash/ProgramVersion.cs
+++ src/FTFlash/ProgramVersion.cs
@@ -1,12 +1,9 @@
-﻿using System.Reflection;
-
-namespace FTFlash;
+﻿namespace FTFlash;
 
 public static class ProgramVersion
 {
-    public static string FullNameAndVersion => $"FTFlash v{VersionString}";
-
-    public static string VersionString => Assembly.GetAssembly(typeof(ProgramVersion))!
-    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()!
-    .InformationalVersion;
+    public const int Major = 1;
+    public const int Minor = 2;
+    public static string ShortString => $"{Major}.{Minor}";
+    public static string FullNameAndVersion => $"FTFlash v{ShortString}";
 }
